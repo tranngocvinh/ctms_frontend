48710c666ab86f0fbfaa1c05d405c4758ecea74c
tran ngoc vinh <vinhvinh307@gmail.com>
2024-05-23 00:03:40 +0700

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+//import './index.css'
+import {Button} from "@chakra-ui/react";
+import SidebarWithHeader from "./Components/SideBar.jsx";
+
+function App() {
+
+
+  return (
+
+      <SidebarWithHeader>
+          <Button>Viết Code vào chỗ nàyq</Button>
+      </SidebarWithHeader>
+
+  )
+}
+
+export default App
