d03aa276bda6cdce5f4eb1c7018eee907124e1f4
Nanh <chibihero2002@gmail.com>
2024-07-17 21:19:50 +0700

    Backend for header round 3

--- .next/static/webpack/webpack.08bbe3671c2a994f.hot-update.js
+++ .next/static/webpack/webpack.08bbe3671c2a994f.hot-update.js
@@ -96,7 +96,7 @@ self["webpackHotUpdate_N_E"]("webpack",{},
 /******/ 
 /******/ /* webpack/runtime/getFullHash */
 /******/ !function() {
-/******/ 	__webpack_require__.h = function() { return "08e1cc36422a888e"; }
+/******/ 	__webpack_require__.h = function() { return "d0c3bed9a8f00d60"; }
 /******/ }();
 /******/ 
 /******/ /* webpack/runtime/global */
